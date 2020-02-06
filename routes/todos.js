48f081a850a75cf29f08d38762080ad19b2ba109
Jovan <jovandjukic2@gmail.com>
2020-02-06 15:58:11 +0100

    Edit todo

--- routes/todos.js
+++ routes/todos.js
@@ -5,13 +5,47 @@ const router = express.Router();
 const db = require("../sequelize");
 const Todo = require("../models/Todo");
 
-/* GET users listing. */
+/* GET todos */
 router.get("/", async (req, res, next) => {
   try {
     const todos = await Todo.findAll({ raw: true });
     console.log(todos);
 
     res.render("todos", { todos });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+/* GET todo */
+router.get("/:id", async (req, res, next) => {
+  try {
+    const todo = await Todo.findByPk(req.params.id, { raw: true });
+    console.log(todo);
+
+    res.render("todo", { todo });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+/* GET todo edit */
+router.get("/edit/:id", async (req, res, next) => {
+  try {
+    const todo = await Todo.findByPk(req.params.id, { raw: true });
+
+    res.render("edit", { todo });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+/* EDIT todo */
+router.post("/edit/:id", async (req, res, next) => {
+  try {
+    await Todo.update(req.body, { where: { id: req.params.id } });
+
+    res.redirect(`/todos/${req.params.id}`);
   } catch (error) {
     throw new Error(error);
   }
